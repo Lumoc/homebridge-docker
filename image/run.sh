fe95e00ec9e9ac61483d31cb7a24fd7fbfd1b194
Lumoc <pigate@web.de>
2017-11-18 22:50:07 +0100

    Update run.sh

--- image/run.sh
+++ image/run.sh
@@ -76,5 +76,5 @@ then
             homebridge ;;
     esac
 else
-    parallel --tag echo {} ::: "homebridge" "homebridge -U ~/.homebridge-hue"
+    parallel echo {} ::: "homebridge" "homebridge -U ~/.homebridge-hue"
 fi
